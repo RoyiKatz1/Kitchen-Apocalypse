1000431b7dd4e41eccb3bb5e9973cb5080d5a536
RoyiKatz1 <royicats@gmail.com>
2025-01-24 17:25:45 +0200

    tomato explosion

--- Assets/Scripts/Tomato.cs
+++ Assets/Scripts/Tomato.cs
@@ -1,6 +1,12 @@
+using UnityEngine;
+
 public class Tomato : Enemy
 {
     public float sizeMultiplier = 1.5f;
+    public float explosionRadius = 2f;
+    public int damage = 20;
+    private Animator animator;
+
 
     void Start()
     {
@@ -20,12 +26,53 @@ public class Tomato : Enemy
         damageInterval *= 2f;
 
         // Change scoring
-        scoreValue = 50; 
+        scoreValue = 50;
+
+        animator = GetComponent<Animator>();
+
     }
 
-    public override void TakeDamage(float damage)
+    protected override void Die()
     {
-        base.TakeDamage(damage);
-        // Additional Tomato-specific damage handling
+        Explode();
+        Debug.Log($"{GetType().Name} died");
+        if (ScoreManager.Instance != null)
+        {
+            ScoreManager.Instance.AddScore(scoreValue, transform.position);
+        }
+        else
+        {
+            Debug.LogWarning("ScoreManager instance not found. Score not added.");
+        }
+
+        Destroy(gameObject, 3f);
+
     }
+
+    public void Explode()
+    {
+        animator.SetTrigger("Explosion");
+
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (Collider2D obj in hitObjects)
+        {
+            if (obj.CompareTag("Player"))
+            {
+                Player player = obj.GetComponent<Player>();
+                if (player != null)
+                {
+                    player.TakeDamage(damage);
+                    Debug.Log($"{GetType().Name} exploded and dealt {damage} damage to the player");
+                }
+            }
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+
 }
