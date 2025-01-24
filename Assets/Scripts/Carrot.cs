32bcb30c96438892e3892a50939774d8c1c9874b
RoyiKatz1 <royicats@gmail.com>
2025-01-24 19:14:19 +0200

    carrot dying animation

--- Assets/Scripts/Carrot.cs
+++ Assets/Scripts/Carrot.cs
@@ -1,9 +1,32 @@
+using UnityEngine;
+
 public class Carrot : Enemy
 {
-    // Carrot specific implementation
-    public override void TakeDamage(float damage)
+
+    private Animator animator;
+
+    protected override void Start()
     {
-        base.TakeDamage(damage);
-        // Additional Carrot-specific damage handling
+        base.Start();
+        animator = GetComponent<Animator>();
     }
+
+    protected override void Die()
+    {
+        AudioManager.Instance.PlaySound(AudioManager.Instance.enemyDeathSound);
+        animator.SetTrigger("Die");
+
+        Debug.Log($"{GetType().Name} died");
+        if (ScoreManager.Instance != null)
+        {
+            ScoreManager.Instance.AddScore(scoreValue, transform.position);
+        }
+        else
+        {
+            Debug.LogWarning("ScoreManager instance not found. Score not added.");
+        }
+        Destroy(gameObject, 0.1f);
+    }
+
+
 }
